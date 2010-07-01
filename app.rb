e2e1e8bdff2f4a3500ef4c426078332c94e3e372
andres <andres@webkoi-collective.com>
2010-07-01 14:25:26 -0400

    Services now properly respond and handle invalid json input

--- app.rb
+++ app.rb
@@ -3,29 +3,42 @@ require 'boot'
 #Haven't made it work as Sinatra::Base in apache/passenger
 #class App < Sinatra::Base
 
+  def criteria_to_json criteria
+    criteria.each.map{ |doc| (response ||= []) << doc }.flatten.to_json
+  end
+
+  def parse_json data
+    begin
+      json = JSON.parse(data)
+    rescue
+      halt 400, "Bad Request"
+    end
+  end
+
   get '/' do
     "Hello there!"
   end
 
   get '/channels' do
     content_type :json
-    Channel.all.each.map {|document| (response ||= []) << document}.to_json
+    criteria_to_json(Channel.all)
   end
 
   get '/sessions' do
     content_type :json
-    Session.all.documents.to_json
+    criteria_to_json(Session.all)
   end
 
   get '/messages' do
     content_type :json
-    Message.all.documents.to_json
+    criteria_to_json(Message.all)
   end
 
   post '/channels' do
     content_type :json
-    params = JSON.parse(request.body.read.to_s)
-    channel = Channel.create(params)
+
+    params = request.body.read
+    channel = Channel.create(parse_json params)
     
     if channel.valid?
       channel.to_json
@@ -36,8 +49,9 @@ require 'boot'
 
   post '/sessions' do
     content_type :json
-    params = JSON.parse(request.body.read.to_s)
-    session = Session.create(params)
+
+    params = request.body.read
+    session = Session.create(parse_json params)
     
     if session.valid?
       session.to_json
@@ -48,8 +62,9 @@ require 'boot'
 
   post '/messages' do
     content_type :json
-    params = JSON.parse(request.body.read.to_s)
-    message = Message.create(params)
+
+    params = request.body.read
+    message = Message.create(parse_json params)
     
     if message.valid?
       message.to_json
--- config.ru
+++ config.ru
@@ -7,6 +7,11 @@ set :environment, ENV['RACK_ENV'].to_sym
 set :root,        root_dir
 set :app_file,    File.join(root_dir, 'app.rb')
 disable :run
+enable :raise_errors
+
+log = File.new("sinatra.log","a")
+STDOUT.reopen(log)
+STDERR.reopen(log)
 
 require 'app'
 
