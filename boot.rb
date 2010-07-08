5af9e6a3370e644088201949ab7fbcf50845e547
Andres Freyria <andres.freyria@gmail.com>
2010-07-07 23:35:55 -0500

    Single quotes where doubles were meant

--- boot.rb
+++ boot.rb
@@ -7,7 +7,7 @@ Bundler.setup
 Bundler.require(:default, SINATRA_ENV)
 
 Mongoid.configure do |config|
-  name = 'rmu_logs_#{SINATRA_ENV}'
+  name = "rmu_logs_#{SINATRA_ENV}"
   host = "localhost"
   config.allow_dynamic_fields = false
   config.use_utc = true
